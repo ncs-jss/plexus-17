fea22fce45c10fbb1ff606c4e25dfc57d7bed0c4
itaditya <adityaa803@gmail.com>
2017-10-10 01:16:52 +0530

    fixes #7

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Landing = () => {
-    return (
-        <div style={{textAlign: 'center'}}>
-            <h1> Plexus-17 ! </h1>
-            Under Development
-            <br />
-            <Link to="/dashboard">Dashboard</Link>
-        </div>
-    )
-};
-
+class Landing extends Component {
+    render() {
+        return (
+          <div style={{textAlign: 'center'}}>
+              <h1> Plexus-17 ! </h1>
+              Under Development
+              <br />
+              <Link to="/dashboard">Dashboard</Link>
+          </div>
+        )
+    }
+}
 export default Landing;
