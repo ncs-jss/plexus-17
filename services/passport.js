39ee1e1265ebfd2c9430c21df7ac58a2d559d440
Aditya Agarwal <adityaa803@gmail.com>
2017-10-17 07:48:09 +0530

    Aditya#async await (#18)
    * used async/await in deserialize
    
    * minor-spacing

--- services/passport.js
+++ services/passport.js
@@ -5,27 +5,32 @@ const config = require('../config');
 const User = require('../models/User');
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
-passport.deserializeUser((id, done) => {
-    User.findById(id).exec((err,user) => {
-        done(null, user);
-    })
-});
-passport.use(new GoogleStrategy({
-    clientID: config.googleClientID,
-    clientSecret: config.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    proxy:true
-}, async (accessToken, refreshToken, profile, done) => {
-    const existingUser = await User.findOne({
-        googleId: profile.id
-    })
-    if (existingUser) {
-        return done(null, existingUser);
-    }
-    const user = await new User({
-        googleId: profile.id
-    }).save();
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
     done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(new GoogleStrategy({
+  clientID: config.googleClientID,
+  clientSecret: config.googleClientSecret,
+  callbackURL: '/auth/google/callback',
+  proxy: true
+}, async (accessToken, refreshToken, profile, done) => {
+  const existingUser = await User.findOne({
+    googleId: profile.id
+  })
+  if (existingUser) {
+    return done(null, existingUser);
+  }
+  const user = await new User({
+    googleId: profile.id
+  }).save();
+  done(null, user);
 }));
