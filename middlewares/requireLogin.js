83232295d4bc7ed40c3b97e71218bc53e303c4b2
itaditya <adityaa803@gmail.com>
2017-10-17 10:56:57 +0530

    formatting

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,8 +1,8 @@
 module.exports = (req, res, next) => {
-    if (!req.user) {
-        return res.status(401).send({
-            error: "You are not Logged In!"
-        });
-    }
-    next();
+  if (!req.user) {
+    return res.status(401).send({
+      error: "You are not Logged In!"
+    });
+  }
+  next();
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,17 +8,23 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireAdminLogin = require('../middlewares/requireAdminLogin');
 
 module.exports = (app) => {
-  app.get('/api/users', requireAdminLogin, joiValidate(userValidator.getList), async (req, res) => {
-    let { limit, skip, fields } = req.items;
+  app.get('/api/users', requireAdminLogin, joiValidate(userValidator.getList), async(req, res) => {
+    let {
+      limit,
+      skip,
+      fields
+    } = req.items;
     limit = parseInt(limit);
     skip = parseInt(skip);
     const users = await User.find().limit(limit).skip(skip);
     res.send(users);
   });
-  app.get('/api/users/:id', requireLogin, joiValidate(userValidator.get), async (req, res) => {
-    let { id } = req.items;
+  app.get('/api/users/:id', requireLogin, joiValidate(userValidator.get), async(req, res) => {
+    let {
+      id
+    } = req.items;
     const user = await User.findById(id);
-    if(user.role === 'admin' && req.user.role !== 'admin'){
+    if (user.role === 'admin' && req.user.role !== 'admin') {
       return res.status(403).send({
         error: 'Access to admin details not allowed'
       })
