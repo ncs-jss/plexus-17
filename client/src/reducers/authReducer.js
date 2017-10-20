841fe3361e20041779926040f4c88a7307eb8a0f
itaditya <adityaa803@gmail.com>
2017-10-20 13:12:44 +0530

    fetch events deleted

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,8 +2,6 @@ import {FETCH_USER, FETCH_EVENTS} from '../actions/types';
 
 export default function (state = null, action) {
   switch (action.type) {
-  case FETCH_EVENTS:
-    return action.payload || false;
   case FETCH_USER:
     return action.payload || false;
   default:
