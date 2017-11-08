b354b21b9e0029b446733b281715dbdfa8e90a2d
Ankit Jain <ankitjain28may77@gmail.com>
2017-11-09 00:54:17 +0530

    Revert "patch #49"

--- middlewares/roleManager.mw.js
+++ middlewares/roleManager.mw.js
@@ -1,12 +1,3 @@
-function isRole(role, req, res, next) {
-  if (!(req.user && req.user.role === role)) {
-    return res.status(401).send({
-      error: `You are not logged in as ${role}!`
-    });
-  }
-  next();
-}
-
 module.exports = {
   isLogin(req, res, next) {
     if (!req.user) {
@@ -16,23 +7,26 @@ module.exports = {
     }
     next();
   },
-  isRole,
-  isAdmin(...args) {
-    return isRole('admin', ...args);
+  isAdmin(req, res, next) {
+    if (!(req.user && req.user.role === 'admin')) {
+      return res.status(401).send({
+        error: 'You are not logged in as admin!'
+      });
+    }
+    next();
   },
-  isManager(...args) {
-    return isRole('manager', ...args);
+  isManager(req, res, next) {
+    if (!(req.user && req.user.role === 'manager')) {
+      return res.status(401).send({
+        error: 'You are not logged in as manager!'
+      });
+    }
+    next();
   },
-  isEditor(...args) {
-    return isRole('editor', ...args);
-  },
-  isSelf(req, res, next) {
-    console.log(req.items);
-  },
-  isAdminOrSelf(req, res, next) {
-    if (req.user.role !== 'admin' && req.user.id !== req.items.id) {
-      return res.status(403).send({
-        error: "You don't have the permission to do this"
+  isEditor(req, res, next) {
+    if (!(req.user && req.user.role === 'editor')) {
+      return res.status(401).send({
+        error: 'You are not logged in as editor!'
       });
     }
     next();
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,38 +1,37 @@
-const router = require('express').Router();
 const passport = require('passport');
 
-router.get('/api/me', (req, res) => {
-  res.send(req.user);
-});
+module.exports = app => {
+  app.get('/api/me', (req, res) => {
+    res.send(req.user);
+  });
 
-router.get('/api/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
-});
+  app.get('/auth/:service', (req, res, next) => {
+    switch (req.params.service) {
+      case 'google':
+        passport.authenticate('google', {
+          scope: ['profile', 'email']
+        })(req, res, next);
+        break;
+      case 'facebook':
+        passport.authenticate('facebook', {
+          scope: ['email', 'user_about_me', 'user_photos']
+        })(req, res, next);
+        break;
+      default:
+        next();
+    }
+  });
 
-router.get('/auth/:service', (req, res, next) => {
-  switch (req.params.service) {
-    case 'google':
-      passport.authenticate('google', {
-        scope: ['profile', 'email']
-      })(req, res, next);
-      break;
-    case 'facebook':
-      passport.authenticate('facebook', {
-        scope: ['email', 'user_about_me', 'user_photos']
-      })(req, res, next);
-      break;
-    default:
-      next();
-  }
-});
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/dashboard');
+  });
 
-router.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-  res.redirect('/dashboard');
-});
+  app.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
+    res.redirect('/dashboard');
+  });
 
-router.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
-  res.redirect('/dashboard');
-});
-
-module.exports = router;
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+};
