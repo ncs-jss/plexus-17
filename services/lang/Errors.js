72c6de205de3bb7cfb6aa70891786bcd12f7978d
Ankit Jain <ankitjain28may77@gmail.com>
2017-11-10 16:20:15 +0530

    Added messages and erros #66

---
+++ services/lang/Errors.js
@@ -0,0 +1,10 @@
+var Errors = {
+  user: {
+    "update": "Error in updating user's data",
+    "create": "Error in creating new user",
+    "edit": "Error in editing user's data",
+    "delete": "Error in deleting user's data"
+  }
+}
+
+module.exports = Error;
---
+++ services/lang/Messages.js
@@ -0,0 +1,10 @@
+var Messages = {
+  user: {
+    "update": "User's data is updated successfully",
+    "create": "New user is created successfully",
+    "edit": "User's data is edited successfully",
+    "delete": "User is deleted successfully"
+  }
+}
+
+module.exports = Messages;
