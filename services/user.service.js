ceaaf31e4fcd96b9daa73ae19f80a20cc476afde
itaditya <adityaa803@gmail.com>
2017-11-10 17:16:30 +0530

    formatted

--- services/user.service.js
+++ services/user.service.js
@@ -5,18 +5,20 @@ const mapPresetToFields = ({ preset, fields }) => {
     profile: 'name username email authId avatar',
     imp: 'name email token username verified flag type role',
     short: '-updatedAt -createdAt -flag -verified -_arena -token'
-  }
+  };
 
   fields = fields || '';
-  
+
   return (preset ? presetMap[preset] : fields).split(' ');
-}
+};
 
 module.exports = {
-  list: async function(options = {
-    limit: 10,
-    skip: 0,
-  }) {
+  list: async function(
+    options = {
+      limit: 10,
+      skip: 0
+    }
+  ) {
     options.limit = parseInt(options.limit);
     options.skip = parseInt(options.skip);
     fields = mapPresetToFields(options);
@@ -35,12 +37,16 @@ module.exports = {
     }
     return new User(data).save();
   },
-  update: async function(id, data, options = {
-    new: true
-  }) {
-    return User.findByIdAndUpdate(id, { $set: data }, options)
+  update: async function(
+    id,
+    data,
+    options = {
+      new: true
+    }
+  ) {
+    return User.findByIdAndUpdate(id, { $set: data }, options);
   },
   remove: async function(id) {
-    return User.findByIdAndRemove(id)
+    return User.findByIdAndRemove(id);
   }
 };
