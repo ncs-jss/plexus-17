a8f719a398badfe646129540558ab5f2f201d37c
itaditya <adityaa803@gmail.com>
2017-11-14 01:33:45 +0530

    format test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,24 +1,23 @@
 const test = require('tape');
 const request = require('supertest')('http://localhost:5000');
 
-
 const cookies = {
   admin: process.env.testAdminCookie,
   user: process.env.testUserCookie
-}
+};
 
 test('GET /users', assert => {
   request
-  .get('/api/users')
-  .query({
-    limit: 10,
-    skip: 0
-  })
-  .set('Cookie', cookies.admin)
-  .expect(200)
-  .expect('Content-Type', /json/)
-  .end((err, res) => {
-    assert.notEqual(res.body, [], 'List of users shouldn\'t be empty');
-    assert.end();
-  });
+    .get('/api/users')
+    .query({
+      limit: 10,
+      skip: 0
+    })
+    .set('Cookie', cookies.admin)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      assert.notEqual(res.body, [], "List of users shouldn't be empty");
+      assert.end();
+    });
 });
