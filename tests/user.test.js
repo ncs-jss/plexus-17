ec17f53c1df4357faebfce32b9c71f6085867e77
Aditya Agarwal <adityaa803@gmail.com>
2017-11-17 11:07:13 +0530

    fixes #89 (#90)

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,17 +3,17 @@ const test = require('tape');
 const userHandler = require('./handlers/user.handler');
 
 test('GET /users', assert => {
-  userHandler.list().end((err, res) => {
-    assert.notEqual(res.body, [], "List of users shouldn't be empty");
+  userHandler.list().end((err, { body: userList }) => {
+    assert.notEqual(userList, [], "List of users shouldn't be empty");
     assert.end();
   });
 });
 
 test('GET /users/:id', assert => {
-  userHandler.list().end((err, { body: userListBody }) => {
-    const user = userListBody[0];
-    userHandler.get(user._id).end((err, { body: userGetBody }) => {
-      assert.notEqual(userGetBody, user, 'Get user must match the one from user list');
+  userHandler.list().end((err, { body: [user] }) => {
+    //user holds the first user document in the userList array
+    userHandler.get(user._id).end((err, { body: userGet }) => {
+      assert.notEqual(userGet, user, 'Get user must match the one from user list');
       assert.end();
     });
   });
