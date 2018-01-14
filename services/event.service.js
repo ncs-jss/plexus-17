4cacb77e88e092e01dc9bd40b4585dd841ede37e
Aditya Agarwal <adityaa803@gmail.com>
2018-01-14 17:52:14 +0530

    fixes #127 (#128)

--- services/event.service.js
+++ services/event.service.js
@@ -1,64 +1,54 @@
 const Event = require('mongoose').model('Event');
 const _isString = require('lodash/isString');
 
-const mapPresetToFields = ({ preset, fields = '' }) => {
-  const presetMap = {
-    profile: 'name,username,email,authId,avatar',
-    imp: 'name,email,token,username,verified,flag,type,role',
-    short: '-__v,-updatedAt,-createdAt,-flag,-verified,-_arena,-token'
-  };
-  return (preset ? presetMap[preset] : fields).split(',');
-};
-
-const getPopulations = include => {
+const getPopulations = ({ include, fields = {} }) => {
   const allowedIncludes = ['_questions'];
   if (_isString(include)) {
     include = [include];
   }
   include = include.filter(includedField => allowedIncludes.includes(includedField));
-  return include.map(includedField => ({ path: includedField }));
+  return include.map(includedField => ({ path: includedField, select: fields[includedField] }));
 };
 
 module.exports = {
-  list: async ({ limit = 10, skip = 0, preset, fields, include = [] }) => {
+  list: async ({ limit = 10, skip = 0, fields = {}, include = [] }) => {
     const options = {};
     options.limit = parseInt(limit);
     options.skip = parseInt(skip);
 
-    fields = mapPresetToFields({ preset, fields });
-    const populations = getPopulations(include);
-
-    return Event.find({}, fields, options).populate(populations);
+    const populations = getPopulations({ include, fields });
+    return Event.find({}, fields.self, options).populate(populations);
   },
-  get: async (id, options) => {
-    fields = mapPresetToFields(options);
-    const { query_field = '_id' } = options;
+  get: async (id, { query_field = '_id', fields = {}, include = [] }) => {
     const query = {};
     query[query_field] = id;
-    return Event.findOne(query, fields);
+
+    const populations = getPopulations({ include, fields });
+    return Event.findOne(query, fields.self).populate(populations);
   },
-  create: async (data, options = {}) => {
-    fields = mapPresetToFields(options);
+  create: async (data, { fields = {} }) => {
     const existingEvent = await Event.findOne(
       {
         name: data.name
       },
-      fields
+      fields.self
     );
     if (existingEvent) {
       return existingEvent;
     }
     return new Event(data).save();
   },
-  update: async (id, data, options = {}) => {
+  update: async (id, data, { fields = {} }) => {
     //todo
-    options.fields = mapPresetToFields(options);
+    const options = {};
+    options.fields = fields.self;
     options.new = true;
     return Event.findByIdAndUpdate(id, { $set: data }, options);
   },
-  remove: async (id, options = {}) => {
+  remove: async (id, { fields = {} }) => {
     //todo
-    options.select = mapPresetToFields(options);
+    const options = {};
+    options.select = fields.self;
     return Event.findByIdAndRemove(id, options);
   },
   addQuestion: async ({ eventId, questionId }) => {
